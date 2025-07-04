783bb03e1a16e83ca7a2cb776b551b9d46323782
Huy Pham <pnc.huy@gmail.com>
2025-07-04 02:47:06 +0000

    Đổi sang font chữ "Mulish"

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['Inter', 'sans-serif'],
-        headline: ['Inter', 'sans-serif'],
+        body: ['Mulish', 'sans-serif'],
+        headline: ['Mulish', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
