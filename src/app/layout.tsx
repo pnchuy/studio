d4357a77096ae0f51592138ef2ad2703a3d42f12
Huy Pham <pnc.huy@gmail.com>
2025-07-04 02:42:52 +0000

    Đổi sang font chữ "inter" đi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
           crossOrigin="anonymous"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Literata:opsz@7..72&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
       </head>
