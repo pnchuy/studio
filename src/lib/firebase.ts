487fb37e39ebe8ccf2a09a7aa42b46ada796053a
Huy Pham <pnc.huy@gmail.com>
2025-08-01 05:11:24 +0000

    điều này có ảnh hưởng gì đến phiên bản đang chạy trên vercel? Hiện tại t

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,13 +5,13 @@ import { getFirestore, type Firestore } from 'firebase/firestore';
 import { getStorage, type FirebaseStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCZsMhSDIcL2yV0GyY0uhPgCz4HypPmIbE",
-  authDomain: "list-read.firebaseapp.com",
-  projectId: "list-read",
-  storageBucket: "list-read.firebasestorage.app",
-  messagingSenderId: "508306002133",
-  appId: "1:508306002133:web:69b85b5c1b3abb9cee125c",
-  measurementId: "G-RGJY257P1Z"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // A flag to check if Firebase is configured
